241f368286c05b260d4581e51ba9359013b02371
WyeneDev <82611242+WyeneDev@users.noreply.github.com>
2021-06-18 21:00:35 +0200

    upload some pretty files

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import requests, AdBlock
+app = Flask(__name__)
+
+@app.route('/url=<path:path>')
+def hello_world(path):
+    path = path.replace('url=','')
+    path = requests.get(path).text
+
+    return AdBlock.Formater(path)
+
+if __name__ == '__main__':
+   app.run(debug=True)
