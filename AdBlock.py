ac9f520aaec24f863ae3da91764c9c9b94601cf0
WyeneDev <82611242+WyeneDev@users.noreply.github.com>
2021-06-21 10:27:29 +0200

    Update AdBlock.py

--- AdBlock.py
+++ AdBlock.py
@@ -1,5 +1,5 @@
 blackList = ['https://connect.facebook.net/','https://www.google-analytics.com/analytics.js','https://improving.duckduckgo.com/','https://www.googletagmanager.com/gtm.js','https://www.googletagservices.com/tag/js/gpt.js', 'https://targeting.api.drift.com']
 def Formater(text):
     for x in blackList:
-        text = text.replace(x,'')
+        text = text.replace(x, '')
     return text
